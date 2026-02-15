9bffb610adfe3be3f18e1205a145ea75e3771bd6
Gustavo Morales <A00828432@tec.mx>
2026-02-14 23:22:49 -0600

    Test Cases and parameter files

--- computeSales.py
+++ computeSales.py
@@ -6,10 +6,11 @@ def main():
     Main entry point
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("priceCataloge", 
+    parser.add_argument("priceCataloge",
                         help= " JSON catalogue of prices of products")
     parser.add_argument("salesRecord",
                         help= "JSON record for all sales in a company")
-    
+
+    start_timer = time.perf_counter()
 if __name__ == "__main__":
     main()
