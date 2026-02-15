8ff4b7a4f49f6e369add990c1b01e114b6d10a69
Gustavo Morales <A00828432@tec.mx>
2026-02-14 22:43:50 -0600

    Adding parse

--- computeSales.py
+++ computeSales.py
@@ -1,2 +1,15 @@
 import argparse
 import time
+
+def main():
+    """
+    Main entry point
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("priceCataloge", 
+                        help= " JSON catalogue of prices of products")
+    parser.add_argument("salesRecord",
+                        help= "JSON record for all sales in a company")
+    
+if __name__ == "__main__":
+    main()
